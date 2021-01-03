fd9523367abd860811947fc6739cfdff2ee985f4
Sripathi Bhat <shripathisimha@gmail.com>
2021-01-03 19:03:06 +0530

    Code cleanup

--- string-calculator/src/main/java/com/bhat/tdd/string_calculator/StringCalculator.java
+++ string-calculator/src/main/java/com/bhat/tdd/string_calculator/StringCalculator.java
@@ -11,25 +11,21 @@ public class StringCalculator {
 		if(numbers.equals("")) {
 			return 0;
 		}
+
 		String delimiter = ",|\n";
+		
+		// Check for custom delimiters
 		if(numbers.startsWith("//")) {
 			Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(numbers);
 			if (matcher.matches()) {
-				// System.out.println(matcher.groupCount() + "\n" + matcher.group(0) + "\n" + matcher.group(1) + "\n" + matcher.group(2));
 				delimiter = matcher.group(1);
 				numbers = matcher.group(2);
-//				String[] tokens = numbers.split(delimiter);
-//				int sum = 0;
-//				if(tokens.length != 0) {
-//					for(int i=0; i<tokens.length; i++) {
-//						sum += Integer.parseInt(tokens[i]);
-//					}
-//				}
-//				return sum;
 			}
 		}
 		
 		int tokens[] = splitNumbers(delimiter, numbers);
+		
+		// Check whether input contains any negative numbers
 		List<Integer> negativeNums = getNegatives(tokens);
 		if(negativeNums.size() > 0) {
 			StringBuilder sb = new StringBuilder();
@@ -40,19 +36,12 @@ public class StringCalculator {
 			sb.append(negativeNums.get(negativeNums.size() - 1));
 			throw new RuntimeException("Negatives not allowed - " + sb);
 		}
-		int sum = calculateSum(tokens);
 		
-//		String[] tokens = numbers.split(",|\n");
-//		int sum = 0;
-//		if(tokens.length != 0) {
-//			for(int i=0; i<tokens.length; i++) {
-//				sum += Integer.parseInt(tokens[i]);
-//			}
-//		}
+		// No negative numbers, so calculate sum
+		int sum = calculateSum(tokens);
 		return sum;
 	}
-	
-	// Created a method to avoid code duplication
+
 	private int[] splitNumbers(String delimiter, String numbers) {
 		String tokens[] = numbers.split(delimiter);
 		int nums[] = new int[tokens.length];
@@ -62,8 +51,7 @@ public class StringCalculator {
 		}
 		return nums;
 	}
-	
-	// Created a method to avoid code duplication
+
 	private int calculateSum(int[] tokens) {
 		int sum = 0;
 		for(int i=0; i<tokens.length; i++) {
@@ -71,7 +59,7 @@ public class StringCalculator {
 		}
 		return sum;
 	}
-	
+
 	private List<Integer> getNegatives(int[] numbers) {
 		List<Integer> nums = new ArrayList<Integer>();
 		for(int num: numbers) {
